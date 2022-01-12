3460697b6f814132fd85f6f6c069c7dbd2c9b97f
StarUndrscre <53362212+StarUndrscre@users.noreply.github.com>
2022-01-12 15:56:50 -0800

    i love video games

--- js/flavor.js
+++ js/flavor.js
@@ -10,7 +10,13 @@ function randomtext() {
         "it\'s pronounced \"star-under-score\"",
         "non-binary rights",
         "the wii was home to many wii games and wii games",
-        "is that a threat?"
+        "is that a threat?",
+        "do you <i>still</i> remember?",
+        "nope, no hints here!",
+        "i love video games",
+        "srt lore",
+        "why is it called \"oisrt tournament\" when its full title<br> is \"\'<b>o</b>nline <b>i</b>nternet <b>s</b>ight <b>r</b>eading <b>t</b>ournament\' tournament\"??????<br>why are there two \
+tournaments\"??????????????????????????",
     ];
     return flavortext[Math.floor((Math.random() * flavortext.length))];
 }
