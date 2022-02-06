4fcd5aac536a005a0af8c984a2f2ed80606054d0
StarUndrscre <davidtigasgameup@gmail.com>
2022-02-05 16:30:29 -0800

    i can now grab mc pack version from the repo

---
+++ js/mc_version.js
@@ -0,0 +1,4 @@
+fetch('https://raw.githubusercontent.com/StarUndrscre/starundrscre-megapack/main/version.txt')
+    .then(response => response.text())
+    .then((data) => document.getElementById("version").innerHTML = (data))
+    .then((data) => console.log(data));
